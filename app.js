f6937d5efb8599ddad8535506f7da54a0991fca7
Lyz03 <lizoe@netcourrier.com>
2021-11-12 11:40:11 +0100

    exo 54

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let y = 4;
 let z = -1;
 
 let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
+let p4 = document.getElementById('p4');
+let p5 = document.getElementById('p5');
 
 if (x < 2) {
     p1.innerText = "x est inférieur à 2";
@@ -13,32 +17,32 @@ else {
 
 
 if (y > 3) {
-    p1.innerText = "y est supérieur à 3";
+    p2.innerText = "y est supérieur à 3";
 }
 else {
-    console.log("false")
+    p2.innerText = "y n'est pas supérieur à 3";
 }
 
 
 if (z <= 0) {
-    console.log("true")
+    p3.innerText = "z est inférieur ou égale à 0";
 }
 else {
-    console.log("false")
+    p3.innerText = "z n'est pas inférieur ou égale à 0";
 }
 
 
 if (x < 2 && y > 3 && z <= 0){
-    console.log("true")
+    p4.innerText = "x est inférieur à 2 et y est supérieur à 3 et z est inférieur ou égale à 0";
 }
 else {
-    console.log("false")
+    p4.innerText = "x n'est pas inférieur à 2 et /ou y n'est pas supérieur à 3 et / ou z n'est pas inférieur ou égale à 0";
 }
 
 
 if (x < 2 && y > 3 || z === -1){
-    console.log("true")
+    p5.innerText = "x est inférieur à 2 et y est supérieur à 3 ou z est strictement égale à -1";
 }
 else {
-    console.log("false")
+    p5.innerText = "x n'est pas inférieur à 2 et / ou y n'est pas supérieur à 3 et z n'est pas strictement égale à -1";
 }
